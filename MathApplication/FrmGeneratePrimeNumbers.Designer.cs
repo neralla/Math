cea8d2ef08e8c5cfa83c5afec2f74be6e4fbbefb
neralla <nerallaprasad@gmail.com>
2016-05-20 21:50:40 -0700

    generate highest prime numbers for 60 seconds and display

---
+++ MathApplication/FrmGeneratePrimeNumbers.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace MathApplication
+{
+    partial class FrmGeneratePrimeNumbers
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnGeneratePrimeNumbers = new System.Windows.Forms.Button();
+            this.SecondsTimer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblSecondsProgress = new System.Windows.Forms.Label();
+            this.lblPrimeNumbers = new System.Windows.Forms.Label();
+            this.txtHighestPrimeNumber = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnGeneratePrimeNumbers
+            // 
+            this.btnGeneratePrimeNumbers.Location = new System.Drawing.Point(210, 96);
+            this.btnGeneratePrimeNumbers.Name = "btnGeneratePrimeNumbers";
+            this.btnGeneratePrimeNumbers.Size = new System.Drawing.Size(188, 23);
+            this.btnGeneratePrimeNumbers.TabIndex = 0;
+            this.btnGeneratePrimeNumbers.Text = "GeneratePrimeNumbers";
+            this.btnGeneratePrimeNumbers.UseVisualStyleBackColor = true;
+            this.btnGeneratePrimeNumbers.Click += new System.EventHandler(this.GeneratePrime_Click);
+            // 
+            // SecondsTimer
+            // 
+            this.SecondsTimer.Tick += new System.EventHandler(this.Secondstimer_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(222, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Seconds ";
+            // 
+            // lblSecondsProgress
+            // 
+            this.lblSecondsProgress.AutoSize = true;
+            this.lblSecondsProgress.Location = new System.Drawing.Point(305, 57);
+            this.lblSecondsProgress.Name = "lblSecondsProgress";
+            this.lblSecondsProgress.Size = new System.Drawing.Size(93, 13);
+            this.lblSecondsProgress.TabIndex = 3;
+            this.lblSecondsProgress.Text = "Seconds Progress";
+            // 
+            // lblPrimeNumbers
+            // 
+            this.lblPrimeNumbers.AutoSize = true;
+            this.lblPrimeNumbers.Location = new System.Drawing.Point(222, 32);
+            this.lblPrimeNumbers.Name = "lblPrimeNumbers";
+            this.lblPrimeNumbers.Size = new System.Drawing.Size(78, 13);
+            this.lblPrimeNumbers.TabIndex = 4;
+            this.lblPrimeNumbers.Text = "Prime Numbers";
+            // 
+            // txtHighestPrimeNumber
+            // 
+            this.txtHighestPrimeNumber.Location = new System.Drawing.Point(306, 24);
+            this.txtHighestPrimeNumber.Name = "txtHighestPrimeNumber";
+            this.txtHighestPrimeNumber.Size = new System.Drawing.Size(144, 20);
+            this.txtHighestPrimeNumber.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(463, 261);
+            this.Controls.Add(this.txtHighestPrimeNumber);
+            this.Controls.Add(this.lblPrimeNumbers);
+            this.Controls.Add(this.lblSecondsProgress);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGeneratePrimeNumbers);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGeneratePrimeNumbers;
+        private System.Windows.Forms.Timer SecondsTimer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblSecondsProgress;
+        private System.Windows.Forms.Label lblPrimeNumbers;
+        private System.Windows.Forms.TextBox txtHighestPrimeNumber;
+    }
+}
+
---
+++ MathApplication/FrmGeneratePrimeNumbers.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+namespace MathApplication
+{
+    public partial class FrmGeneratePrimeNumbers : Form
+    {
+        public FrmGeneratePrimeNumbers()
+        {
+            InitializeComponent();
+            
+        }
+        private int counter;
+        private CancellationToken token;
+        private CancellationTokenSource tokensource  ;
+        private int timeInSeconds = 60;
+        private long largestPrime=7;
+        private void GeneratePrime_Click(object sender, EventArgs e)
+        {
+            counter = 0;
+            tokensource = new CancellationTokenSource();
+            token = tokensource.Token;
+            ShowProgressInSeconds();
+            GetLargestPrime(token);
+            btnGeneratePrimeNumbers.Enabled = false;
+        }
+
+        private void Secondstimer_Tick(object sender, EventArgs e)
+        {
+
+            if (counter < timeInSeconds)
+            {
+                counter++;
+                lblSecondsProgress.Text = counter.ToString();
+                txtHighestPrimeNumber.Text = largestPrime.ToString();
+            }
+            else
+            {
+                SecondsTimer.Enabled = false;
+                tokensource.Cancel();
+                btnGeneratePrimeNumbers.Enabled = true;
+            }
+        }
+        public void ShowProgressInSeconds()
+        {
+            if (!SecondsTimer.Enabled)
+            {
+                lblSecondsProgress.Text = counter.ToString();
+                SecondsTimer.Interval =  1000;// milliSeconds / 1000;
+                SecondsTimer.Enabled = true;
+            }
+        }
+        private  long GetLargestPrime(CancellationToken token)
+        {
+            try
+            {           
+                 Task t = Task.Factory.StartNew(() =>
+                {
+
+                    long numberToVerify = largestPrime;
+                        while (1 == 1)
+                        {
+                            if (token.IsCancellationRequested)
+                            {
+                                break;
+                            }
+                            else
+                            {
+                                numberToVerify++;
+                                if (IsPrime(numberToVerify))
+                                {
+                                    largestPrime = numberToVerify;
+                                }
+                            }
+                        }                
+
+                }, token);
+                t.Wait(timeInSeconds);
+                return largestPrime;
+            }
+            catch (AggregateException agEx)
+            {
+                if (agEx.InnerException is OutOfMemoryException)
+                    throw new Exception(string.Format("Task error occured as value of highest prime exceeded the range of long"));
+                else
+                    throw agEx.InnerException;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        private static bool IsPrime(long input)
+        {
+
+            if (input % 2 == 0)
+            {
+                return false;
+            }
+            for (long i = 3; i * i <= input; i += 2)
+            {
+                if ((input % i) == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
